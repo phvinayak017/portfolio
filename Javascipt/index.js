d20176e872ef8260137f43faec77e20536083281
vinayakph017 <vinayak.hulabutti@gmail.com>
2019-11-21 14:17:58 +0530

    css

--- Javascipt/index.js
+++ Javascipt/index.js
@@ -16,4 +16,44 @@ const navSlide = () => {
   })
 }
 
+
+const mywork= document.querySelector('.cta-resume')
+const github = document.querySelector('#github')
+const linkedin = document.querySelector('#linkedin')
+const medium = document.querySelector("#medium")
+const behance = document.querySelector("#behance")
+
+
+const handleResumeButton = () =>{
+  mywork.addEventListener("click", ()=>{
+    window.open("https://drive.google.com/file/d/1EsEy_lpfsoU6snBJK5sF0WzdaWUcWFdA/view?usp=sharing");
+  })
+}
+
+const handleGitSocial = () =>{
+  github.addEventListener("click", () =>{
+    window.open("https://github.com/phvinayak017?tab=repositories")
+  })
+}
+
+const handleLinkedInSocial = () =>{
+  linkedin.addEventListener("click", () =>{
+    window.open("https://www.linkedin.com/in/vinayak-hulabutti-593a7636/")
+  })
+}
+
+const handleMediumSocial  = () =>{
+  medium.addEventListener("click", () =>{
+    window.open("https://medium.com/@ph_vinayak")
+  })
+}
+
+const handleBehanceSocial = () =>{
+  behance.addEventListener("click", () =>{
+    window.open("https://www.behance.net/vinayakhulb31f")
+  })
+}
+
+
 navSlide()
+
