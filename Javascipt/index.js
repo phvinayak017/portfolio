81becb4cf3dfdd2be20e7ac0e2811f94a705d160
vinayakph017 <vinayak.hulabutti@gmail.com>
2019-11-26 14:02:41 +0530

    css

--- Javascipt/index.js
+++ Javascipt/index.js
@@ -17,43 +17,49 @@ const navSlide = () => {
 }
 
 
-const mywork= document.querySelector('.cta-resume')
-const github = document.querySelector('#github')
-const linkedin = document.querySelector('#linkedin')
-const medium = document.querySelector("#medium")
-const behance = document.querySelector("#behance")
 
 
 const handleResumeButton = () =>{
+  const mywork= document.querySelector('.cta-resume')
   mywork.addEventListener("click", ()=>{
     window.open("https://drive.google.com/file/d/1EsEy_lpfsoU6snBJK5sF0WzdaWUcWFdA/view?usp=sharing");
   })
 }
 
 const handleGitSocial = () =>{
+  const github = document.querySelector('#github')
   github.addEventListener("click", () =>{
+    console.log("is open")
     window.open("https://github.com/phvinayak017?tab=repositories")
   })
 }
 
 const handleLinkedInSocial = () =>{
+  const linkedin = document.querySelector('#linkedin')
   linkedin.addEventListener("click", () =>{
     window.open("https://www.linkedin.com/in/vinayak-hulabutti-593a7636/")
   })
 }
 
 const handleMediumSocial  = () =>{
+  const medium = document.querySelector("#medium")
   medium.addEventListener("click", () =>{
-    window.open("https://medium.com/@ph_vinayak")
+    window.open("https://www.medium.com/@ph_vinayak")
   })
 }
 
 const handleBehanceSocial = () =>{
+  const behance = document.querySelector("#behance")
   behance.addEventListener("click", () =>{
     window.open("https://www.behance.net/vinayakhulb31f")
   })
 }
 
+handleGitSocial()
+handleLinkedInSocial()
+handleMediumSocial()
+handleBehanceSocial()
+handleResumeButton()
 
 navSlide()
 
